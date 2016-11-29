fc3551db7e85b1098b1f9abd9b79636cb8b3fbb2
weissercn <weissercn@gmail.com>
2016-11-29 12:57:12 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='spearmint_wrapper',
+      version='0.1',
+      description='A wrapper around the spearmint gaussian process to optimise hyperparameters for skearn, xgboost and keras',
+      url='https://github.com/weissercn/spearmint_wrapper',
+      author='Constantin Weisser',
+      author_email='weissercn@gmail.com',
+      license='MIT',
+      packages=['spearmint_wrapper'],
+      zip_safe=False)
+
+
---
+++ spearmint_wrapper/__init__.py
@@ -0,0 +1,5 @@
+import .optimise_hyperparam
+import .setup_optimise_hyperparam
+from .optimise_hyperparam import spearmint_example
+
+
